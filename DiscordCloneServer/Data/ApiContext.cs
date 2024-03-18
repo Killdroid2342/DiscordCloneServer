55e2d86b7eb69c7c96e02f1f63c84f0da515b120
Haroon Khan <HaroonKhan120704@gmail.com>
2024-03-18 16:41:46 +0000

    created new file

---
+++ DiscordCloneServer/Data/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using DiscordCloneServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordCloneServer.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<RegisterAccount> Accounts { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options)
+             : base(options)
+        {
+
+        }
+    }
+}
