05c3ef5a188a47bf087c55bf11e9952efc80fd29
Haroon Khan <HaroonKhan120704@gmail.com>
2024-03-18 16:41:54 +0000

    created new file

---
+++ DiscordCloneServer/Models/RegisterAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscordCloneServer.Models
+{
+    public class RegisterAccount
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
