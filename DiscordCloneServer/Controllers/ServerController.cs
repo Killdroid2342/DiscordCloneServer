cdaf985e4067d6b1204a2c5a4f9a596fccbc1ab6
Haroon Khan <HaroonKhan120704@gmail.com>
2024-04-23 10:08:41 +0100

    added controller

---
+++ DiscordCloneServer/Controllers/ServerController.cs
@@ -0,0 +1,31 @@
+﻿using DiscordCloneServer.Data;
+using DiscordCloneServer.Migrations;
+using DiscordCloneServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscordCloneServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServerController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        private readonly IConfiguration _config;
+        public ServerController(ApiContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        // create/edit
+        [HttpPost]
+        public JsonResult CreateServer(Models.CreateServer createServer)
+        {
+            Console.WriteLine("this line was executed ");
+
+            _context.CreateServers.Add(createServer);
+
+            return new JsonResult(createServer);
+        }
+    }
+
+}
