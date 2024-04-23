500d612536b4a32156400147c3461b48b1d3fd84
Haroon Khan <HaroonKhan120704@gmail.com>
2024-04-23 10:10:06 +0100

    added model for create server

---
+++ DiscordCloneServer/Models/CreateServer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiscordCloneServer.Models
+{
+    public class CreateServer
+    {
+        [Key]
+        public string ServerName { get; set; }
+        public string ServerID { get; set; }
+        public string ServerOwner { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
