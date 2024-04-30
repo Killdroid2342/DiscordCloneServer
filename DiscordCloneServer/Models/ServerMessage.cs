d10ad55fee1df52253e029e32284c6892a64dc04
Haroon Khan <HaroonKhan120704@gmail.com>
2024-04-30 22:05:51 +0100

    created new file

---
+++ DiscordCloneServer/Models/ServerMessage.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiscordCloneServer.Models
+{
+    public class ServerMessage
+    {
+        [Key]
+        public string MessageID { get; set; }
+        public string ServerID { get; set; }
+        public string ServerName { get; set; }
+        public string MessagesUserSender { get; set; }
+        public string Date { get; set; }
+    }
+}
