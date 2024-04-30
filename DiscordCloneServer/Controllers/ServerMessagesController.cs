c70adc7c922c4e7d7a31678d9e8dcbaa047df6b4
Haroon Khan <HaroonKhan120704@gmail.com>
2024-04-30 22:06:05 +0100

    created post request

---
+++ DiscordCloneServer/Controllers/ServerMessagesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using DiscordCloneServer.Data;
+using DiscordCloneServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscordCloneServer.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ServerMessagesController : Controller
+    {
+        private readonly ApiContext _context;
+        private readonly IConfiguration _config;
+        public ServerMessagesController(ApiContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        [HttpPost]
+        public JsonResult ServerMessages(Models.ServerMessage serverMessage)
+        {
+
+            _context.ServerMessages.Add(serverMessage);
+
+            _context.SaveChanges();
+            return new JsonResult(serverMessage);
+        }
+
+        [HttpGet]
+        public JsonResult GetServerMessages(string username)
+        {
+            return new JsonResult(username);
+        }
+    }
+}
