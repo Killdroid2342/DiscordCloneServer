70caf64ca8dd33951435e0b0b3f1aa8861a753b4
Haroon Khan <HaroonKhan120704@gmail.com>
2024-05-01 17:17:05 +0100

    fixed migrations

---
+++ DiscordCloneServer/Migrations/20240501140523_Server_Message.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscordCloneServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Server_Message : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Server_Message",
+                columns: table => new
+                {
+                    MessageID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ServerID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessagesUserSender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    userText = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Server_Message", x => x.MessageID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Server_Message");
+        }
+    }
+}
