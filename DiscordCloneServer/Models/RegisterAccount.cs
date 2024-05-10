c010f1f9d7919c06e4d72b22c76ab62d5acfef79
Haroon Khan <HaroonKhan120704@gmail.com>
2024-05-10 13:24:46 +0100

    fixing migration issues

--- DiscordCloneServer/Models/RegisterAccount.cs
+++ DiscordCloneServer/Models/RegisterAccount.cs
@@ -6,6 +6,8 @@
         public int Id { get; set; }
         public string UserName { get; set; }
         public string PassWord { get; set; }
+        public string[] Following { get; set; }
+        public string[] Followers { get; set; }
 
     }
 }
