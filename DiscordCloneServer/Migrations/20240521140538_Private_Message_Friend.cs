0029b0d41a4dfecf0005d2db459a64857f3a343d
Haroon Khan <HaroonKhan120704@gmail.com>
2024-05-21 15:24:23 +0100

    added new migrations

---
+++ DiscordCloneServer/Migrations/20240521140538_Private_Message_Friend.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscordCloneServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Private_Message_Friend : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Private_Message_Friend",
+                columns: table => new
+                {
+                    MessageId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    friendMessagesData = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageUserReciver = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessagesUserSender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Private_Message_Friend", x => x.MessageId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Private_Message_Friend");
+        }
+    }
+}
