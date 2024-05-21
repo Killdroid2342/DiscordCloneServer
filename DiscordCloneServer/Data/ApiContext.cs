a821ea8fa72ca71c3651e32c26f8437ea05aa403
Haroon Khan <HaroonKhan120704@gmail.com>
2024-05-21 15:24:02 +0100

    created new dbset

--- DiscordCloneServer/Data/ApiContext.cs
+++ DiscordCloneServer/Data/ApiContext.cs
@@ -9,6 +9,7 @@ namespace DiscordCloneServer.Data
         public DbSet<Account> Accounts { get; set; }
         public DbSet<CreateServer> CreateServers { get; set; }
         public DbSet<ServerMessage> ServerMessages { get; set; }
+        public DbSet<PrivateMessageFriend> PrivateMessageFriends { get; set; }
         public ApiContext(DbContextOptions<ApiContext> options)
             : base(options)
         {
@@ -24,6 +25,7 @@ namespace DiscordCloneServer.Data
             modelBuilder.Entity<Account>().ToTable("Accounts");
             modelBuilder.Entity<CreateServer>().ToTable("Create_Server");
             modelBuilder.Entity<ServerMessage>().ToTable("Server_Message");
+            modelBuilder.Entity<PrivateMessageFriend>().ToTable("Private_Message_Friend");
             base.OnModelCreating(modelBuilder);
             Console.WriteLine("this line was read");
 
