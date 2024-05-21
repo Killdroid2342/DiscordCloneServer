44d9c2554437b83792988eded903484c13fc1a31
Haroon Khan <HaroonKhan120704@gmail.com>
2024-05-21 15:23:50 +0100

    created new model

---
+++ DiscordCloneServer/Models/PrivateMessageFriend.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiscordCloneServer.Models
+{
+    public class PrivateMessageFriend
+    {
+        [Key]
+        public string MessageId { get; set; }
+        public string friendMessagesData { get; set; }
+        public string MessageUserReciver { get; set; }
+        public string MessagesUserSender { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
