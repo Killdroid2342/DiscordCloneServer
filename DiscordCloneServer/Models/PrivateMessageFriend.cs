0bb5ff7c50e387a7cd22060dda3a702e72490f47
Haroon Khan <HaroonKhan120704@gmail.com>
2024-05-21 16:31:56 +0100

    issues when setting ID fixed

--- DiscordCloneServer/Models/PrivateMessageFriend.cs
+++ DiscordCloneServer/Models/PrivateMessageFriend.cs
@@ -5,11 +5,11 @@ namespace DiscordCloneServer.Models
     public class PrivateMessageFriend
     {
         [Key]
-        public string MessageId { get; set; }
-        public string friendMessagesData { get; set; }
+        public string PrivateMessageID { get; set; } = Guid.NewGuid().ToString();
+        public string FriendMessagesData { get; set; }
         public string MessageUserReciver { get; set; }
         public string MessagesUserSender { get; set; }
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
 
     }
 }
